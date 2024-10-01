6e0e575558eb09213edf28a9aab76ac6be119c8b
“datgfg” <dat.le@global-fashion-group.com>
2024-10-01 22:15:09 +0700

    gemini

--- litellm/llms/vertex_ai_and_google_ai_studio/gemini/vertex_and_google_ai_studio_gemini.py
+++ litellm/llms/vertex_ai_and_google_ai_studio/gemini/vertex_and_google_ai_studio_gemini.py
@@ -1420,6 +1420,7 @@ class ModelResponseIterator:
     def _common_chunk_parsing_logic(self, chunk: str) -> GenericStreamingChunk:
         try:
             chunk = chunk.replace("data:", "")
+            print(f'xxxxxx {chunk}')
             if len(chunk) > 0:
                 """
                 Check if initial chunk valid json
